e144f90b21dd5eca46332b12e1d0881d5ddfd356
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-01-19 20:46:07 +0530

    this was my 1st code.(Hello_World)
    basic code.

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package one_using_scanner_and_with_basics;
+
+public class HelloWorld {
+	public static void main(String[]args)
+	{
+		System.out.println("hello world its my first java project");
+	}
+
+}
