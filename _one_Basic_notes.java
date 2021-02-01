a535e0009b9a3a872362f83febf5837d3a30dd75
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-02-01 10:02:14 +0530

    basic notes of Conditional operatoe
    here we have if else and else if also ternary operator

---
+++ _one_Basic_notes.java
@@ -0,0 +1,32 @@
+//there are conditional operations -->
+// 1. if
+// 2. ifelse
+// 3. else 
+// 
+// syntax of these conditional statments -->
+
+
+// 1. if --->
+//if (condition)
+//{
+//}
+
+
+//2.else if(condition)
+//{
+//}
+
+
+
+//3.else
+//{
+//}
+///////////////////////////////////////////SHORTCUT METHOD FOR IF ELSE //////////////////////////////
+///////////////////////////////////////////ternary operator//////////////////////////////////////////
+//<var.type>Variable_name = condition?<expression true>:<expression false>
+
+//eg:-
+//int a = 10;
+//int b= 100;
+//int c = a<b? a:b
+//System.out.println(+c);        // it will print c to be 10.
