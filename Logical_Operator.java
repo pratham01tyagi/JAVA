a7ea45b6b516e1c4d33fd96b5a4148d2c467d51b
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-02-02 11:52:20 +0530

    incompleate logical operator..
    this needs to be continued.

---
+++ Logical_Operator.java
@@ -0,0 +1,14 @@
+package two_operatorsUse;
+
+public class Logical_Operator {
+
+	public static void main(String[] args) {
+		// there are 3 types of logical operators 
+		//1.AND  &&
+		//2.OR   ||
+		//3.NOT   !
+		
+
+	}
+
+}
