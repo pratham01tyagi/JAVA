ba52decdabf45c3b61823f3963b63afc578fa2a2
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-02-18 22:20:57 +0530

    5 strings --> basic notes..
    these are just Basic notes on Strings and they are based on Strings topic no.5

---
+++ Strings_basic_notes.java
@@ -0,0 +1,18 @@
+//strings are basically improvised version of arrays.
+// they are initialised in two ways and there initialization determine the place where it would be storing the string .
+
+
+//they are initialized in two ways.
+
+//String<String name> = "< enter string here >";
+//this will store data or string in the STRING POOL AREA .... it is called Initialized by literals.
+
+//String <String name>= new String("<enter string here>");
+//this will store data in heap area.
+
+
+
+
+
+
+////////////////Declaring a string by string literal is good as it optimizes the memory required by our programe... 
